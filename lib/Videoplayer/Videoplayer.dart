da6cf67174af96c6086f08bd4ba0dc82f95ffccd
user <chauhangautam496@gmail.com>
2024-01-11 11:25:43 +0530

    this my first

--- lib/Videoplayer/Videoplayer.dart
+++ lib/Videoplayer/Videoplayer.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
 class VideoPlayerExample extends StatefulWidget {
 const VideoPlayerExample({Key? key}) : super(key: key);
